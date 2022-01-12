9bd23f7ec4fa3101593bedf04fc395aa7dd0dc1a
EvgeniiDB <doktor-dell@mail.ru>
2022-01-12 17:51:18 +0300

    constraints first view

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Евгений Доброволец on 11.01.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+    
+    
+}
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
     }
-
-
+    
 }
 
