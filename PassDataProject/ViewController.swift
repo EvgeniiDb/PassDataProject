0e659ab74dbc0dd710eceb22e57f8eca016b4a8d
EvgeniiDB <doktor-dell@mail.ru>
2022-01-12 20:18:39 +0300

    segue 1 level

--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -12,11 +12,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var loginTF: UITextField!
     
+    
     @IBOutlet weak var passwordTF: UITextField!
     
     
     @IBAction func loginTapped(_ sender: UIButton) {
+
+            performSegue(withIdentifier: "detailSegue", sender: nil)
+      
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     
 }
 
